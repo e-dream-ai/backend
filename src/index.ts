457d96d2034584847039f9f85a97e70da85d99ea
aurelio <aurelio.olive@gmail.com>
2023-08-25 10:47:36 -0300

    feat: postgres

--- src/index.ts
+++ src/index.ts
@@ -4,18 +4,23 @@ import express, { Request, Response, NextFunction } from "express";
 // const swaggerUi = require('swagger-ui-express');
 // import swaggerDoc from "./swagger";
 import { json } from "body-parser";
+import { PostgresService } from "./infra/postgres.service";
+import { validateEnv } from "./shared/validateEnv";
+
+validateEnv();
 
 const app: express.Application = express();
 const port = process.env.PORT ?? 8080;
-const version = process.env.npm_package_version
+const version = process.env.npm_package_version;
+const postgresService = new PostgresService();
 
-const routes = []
+const routes = [];
 
 const customHeaders = (req: Request, res: Response, next: NextFunction) => {
-    app.disable("x-powered-by");
-    res.setHeader("X-Powered-By", `e-dream.ai ${version}`);
-    next();
-}
+  app.disable("x-powered-by");
+  res.setHeader("X-Powered-By", `e-dream.ai ${version}`);
+  next();
+};
 
 app.use(json());
 app.use(cors());
@@ -24,11 +29,12 @@ app.use(customHeaders);
 routes.push();
 
 app.get("/", (req: Request, res: Response) => {
-    res
-        .status(200)
-        .send({ message: `e-dream.ai is running api at version ${version}` });
+  res
+    .status(200)
+    .send({ message: `e-dream.ai is running api at version ${version}` });
 });
 
-app.listen(port, () => {
-    console.debug(`e-dream.ai api ${version} started on port ${port}`);
+app.listen(port, async () => {
+  console.debug(`e-dream.ai api ${version} started on port ${port}`);
+  await postgresService.connect();
 });

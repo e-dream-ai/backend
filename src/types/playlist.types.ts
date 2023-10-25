06e7c0e15da7289d2c5e5f0bd648496fd9570f9d
Alejandro Casillas <ealejandrocasillas@gmail.com>
2023-10-25 09:30:19 -0700

    feat(playlist): playlist endpoints to add items to playlist

--- src/types/playlist.types.ts
+++ src/types/playlist.types.ts
@@ -1,9 +1,20 @@
+export enum PlaylistItemType {
+  NONE = "none",
+  PLAYLIST = "playlist",
+  DREAM = "dream",
+}
+
 export type CreatePlaylistRequest = {
   name?: string;
   thumbnail?: string;
 };
 
-export type UpdatePlaylistRequest = {
-  name?: string;
-  thumbnail?: string;
+export type UpdatePlaylistRequest = Array<{
+  id: number;
+  order: number;
+}>;
+
+export type AddPlaylistItemRequest = {
+  type: PlaylistItemType;
+  id: number;
 };

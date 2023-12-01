e2af797cbff15dc4ae6eaeb529a63e64c9b26516
Alejandro Casillas <ealejandrocasillas@gmail.com>
2023-12-01 14:43:38 -0700

    feat(profile): user profile feedback fixes

--- src/constants/file.constants.ts
+++ src/constants/file.constants.ts
@@ -1,10 +1,20 @@
-export const MEDIA_TYPES = {
+export const MYME_TYPES = {
   MP4: "video/mp4",
+  JPG: "image/jpeg",
   JPEG: "image/jpeg",
+  PNG: "image/png",
 };
 
 export const FILE_EXTENSIONS = {
   MP4: "mp4",
   JPG: "jpg",
-  JPEG: "JPEG",
+  JPEG: "jpeg",
+  PNG: "png",
+};
+
+export const MYME_TYPES_EXTENSIONS = {
+  [MYME_TYPES.MP4]: FILE_EXTENSIONS.MP4,
+  [MYME_TYPES.JPG]: FILE_EXTENSIONS.JPG,
+  [MYME_TYPES.JPEG]: FILE_EXTENSIONS.JPEG,
+  [MYME_TYPES.PNG]: FILE_EXTENSIONS.PNG,
 };

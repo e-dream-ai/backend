ba5679782a0f370f7f20543c18204153678477d6
Alejandro Casillas <ealejandrocasillas@gmail.com>
2023-12-08 14:54:41 -0700

    feat(user): adding user roles #32
    * feat(user): adding user roles
    
    * feat(user): user roles handlers working on controllers

--- src/routes/v1/feed.router.ts
+++ src/routes/v1/feed.router.ts
@@ -1,11 +1,23 @@
+import { ROLES } from "constants/role.constants";
 import * as feedController from "controllers/feed.controller";
 import { Router } from "express";
 import { requireAuth } from "middlewares/require-auth.middleware";
+import { checkRoleMiddleware } from "middlewares/role.middleware";
 
 const feedRouter = Router();
 
-feedRouter.get("/", requireAuth, feedController.handleGetFeed);
+feedRouter.get(
+  "/",
+  requireAuth,
+  checkRoleMiddleware([ROLES.USER_GROUP, ROLES.ADMIN_GROUP]),
+  feedController.handleGetFeed,
+);
 
-feedRouter.get("/my-dreams", requireAuth, feedController.handleGetMyDreams);
+feedRouter.get(
+  "/my-dreams",
+  requireAuth,
+  checkRoleMiddleware([ROLES.USER_GROUP, ROLES.ADMIN_GROUP]),
+  feedController.handleGetMyDreams,
+);
 
 export default feedRouter;
--- src/schemas/user.schema.ts
+++ src/schemas/user.schema.ts
@@ -1,9 +1,16 @@
+import { ROLES } from "constants/role.constants";
 import Joi from "joi";
-import { UpdateUserRequest } from "types/user.types";
+import { UpdateUserRequest, UpdateUserRoleRequest } from "types/user.types";
 
 export const updateUserSchema = {
   body: Joi.object<UpdateUserRequest>().keys({
     name: Joi.string().required().max(50),
     description: Joi.string().max(300),
+  }),
+};
+
+export const updateUserRoleSchema = {
+  body: Joi.object<UpdateUserRoleRequest>().keys({
+    role: Joi.string().required().valid(ROLES.ADMIN_GROUP, ROLES.USER_GROUP),
   }),
 };

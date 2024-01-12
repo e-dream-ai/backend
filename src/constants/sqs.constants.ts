5e1a97ed1b9300845c1465ef61d7c72aa00b55bc
Alejandro Casillas <ealejandrocasillas@gmail.com>
2024-01-12 15:33:06 -0700

    feat(dream): sqs process video (#43)
    * feat(dream): sqs process video send message
    
    * feat(dream): adding process status to dream
    
    * feat(dream): adding status update endpoints

---
+++ src/constants/sqs.constants.ts
@@ -0,0 +1,12 @@
+export const PROCESS_VIDEO_QUEUE_ATTRIBUTES = {
+  UUID: "UUID",
+  VIDEO: "VIDEO",
+  USER_UUID: "USER_UUID",
+};
+
+export const SQS_DATA_TYPES = {
+  STRING: "String",
+  NUMBER: "Number",
+  BINARY: "Binary",
+  CUSTOM: "Custom",
+};
---
+++ src/utils/dream.util.ts
@@ -0,0 +1,42 @@
+import { Dream } from "entities";
+import {
+  SendMessageCommand,
+  SendMessageCommandInput,
+} from "@aws-sdk/client-sqs";
+import { sqsClient } from "clients/sqs.client";
+import {
+  PROCESS_VIDEO_QUEUE_ATTRIBUTES,
+  SQS_DATA_TYPES,
+} from "constants/sqs.constants";
+import env from "shared/env";
+
+const queueUrl = env.AWS_SQS_URL;
+
+/**
+ * Send dream to sqs queue
+ * @param dream - dream should include contain user data
+ */
+export const processDreamSQS = async (dream: Dream) => {
+  const input: SendMessageCommandInput = {
+    QueueUrl: queueUrl,
+    MessageGroupId: dream.uuid,
+    MessageBody: dream.uuid,
+    MessageAttributes: {
+      [PROCESS_VIDEO_QUEUE_ATTRIBUTES.UUID]: {
+        StringValue: dream.uuid,
+        DataType: SQS_DATA_TYPES.STRING,
+      },
+      [PROCESS_VIDEO_QUEUE_ATTRIBUTES.VIDEO]: {
+        StringValue: dream.video ?? "",
+        DataType: SQS_DATA_TYPES.STRING,
+      },
+      [PROCESS_VIDEO_QUEUE_ATTRIBUTES.USER_UUID]: {
+        StringValue: dream.user.cognitoId ?? "",
+        DataType: SQS_DATA_TYPES.STRING,
+      },
+    },
+  };
+
+  const command = new SendMessageCommand(input);
+  await sqsClient.send(command);
+};

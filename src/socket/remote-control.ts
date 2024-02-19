06c18181fc73b1fe11a082a74f1e1cd8071f5b0b
dealparadize <ealejandrocasillas@gmail.com>
2024-02-19 14:09:29 -0700

    feat(socket.io): adding user log to socket events

--- src/socket/remote-control.ts
+++ src/socket/remote-control.ts
@@ -3,18 +3,24 @@ import { APP_LOGGER } from "shared/logger";
 import { Socket } from "socket.io";
 import { Event } from "socket.io/dist/socket";
 
-const REMOTE_CONTROL_EVENT = "remote_control_event";
 const NEW_REMOTE_CONTROL_EVENT = "new_remote_control_event";
 
 export const remoteControlConnectionListener = async (socket: Socket) => {
   const user: User = socket.data.user;
-  APP_LOGGER.emit(`User ${user?.cognitoId} connected to socket.io`);
+  APP_LOGGER.info(`User ${user?.cognitoId} connected to socket.io`);
   const roomId = "user-" + user.cognitoId;
   socket.join(roomId);
-  socket.on(REMOTE_CONTROL_EVENT, remoteControlEventListener(socket, roomId));
+  socket.on(
+    NEW_REMOTE_CONTROL_EVENT,
+    remoteControlEventListener(socket, roomId, user),
+  );
 };
 
 export const remoteControlEventListener =
-  (socket: Socket, roomId: string) => (data: Event) => {
+  (socket: Socket, roomId: string, user: User) => (data: Event) => {
+    APP_LOGGER.info(
+      `User ${user?.cognitoId} sent ${NEW_REMOTE_CONTROL_EVENT}`,
+      { data },
+    );
     socket.broadcast.to(roomId).emit(NEW_REMOTE_CONTROL_EVENT, data);
   };

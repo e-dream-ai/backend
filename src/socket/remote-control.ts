c4f353b2d5668b4dde7adc38198d6d4710b4acb7
dealparadize <ealejandrocasillas@gmail.com>
2024-02-19 14:47:43 -0700

    feat(socket.io): temporal user logging

--- src/socket/remote-control.ts
+++ src/socket/remote-control.ts
@@ -1,5 +1,4 @@
 import { User } from "entities";
-import { APP_LOGGER } from "shared/logger";
 import { Socket } from "socket.io";
 import { Event } from "socket.io/dist/socket";
 
@@ -7,7 +6,10 @@ const NEW_REMOTE_CONTROL_EVENT = "new_remote_control_event";
 
 export const remoteControlConnectionListener = async (socket: Socket) => {
   const user: User = socket.data.user;
-  APP_LOGGER.info(`User ${user?.cognitoId} connected to socket.io`);
+  /**
+   * Temporal info log
+   */
+  console.info(`User ${user?.cognitoId} connected to socket.io`);
   const roomId = "user-" + user.cognitoId;
   socket.join(roomId);
   socket.on(
@@ -18,9 +20,11 @@ export const remoteControlConnectionListener = async (socket: Socket) => {
 
 export const remoteControlEventListener =
   (socket: Socket, roomId: string, user: User) => (data: Event) => {
-    APP_LOGGER.info(
-      `User ${user?.cognitoId} sent ${NEW_REMOTE_CONTROL_EVENT}`,
-      { data },
-    );
+    /**
+     * Temporal info log
+     */
+    console.info(`User ${user?.cognitoId} sent ${NEW_REMOTE_CONTROL_EVENT}`, {
+      data,
+    });
     socket.broadcast.to(roomId).emit(NEW_REMOTE_CONTROL_EVENT, data);
   };

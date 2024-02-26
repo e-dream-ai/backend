dc8f3fea1fa7930691953d315595774feffdfe10
Max Carlson <>
2024-02-26 11:11:36 +0200

    Add basic error tracking

--- src/index.ts
+++ src/index.ts
@@ -10,8 +10,16 @@ import {
 } from "middlewares/middleware";
 import { registerRoutes } from "routes/v1/router";
 import { remoteControlConnectionListener } from "socket/remote-control";
+import Bugsnag from "@bugsnag/js";
+import BugsnagPluginExpress from "@bugsnag/plugin-express";
+
+Bugsnag.start({
+  apiKey: "eb986254778e3ae5b0f7045ec7fdb9ec",
+  plugins: [BugsnagPluginExpress],
+});
 
 const app: express.Application = express();
+const bugsnagmiddleware = Bugsnag.getPlugin("express");
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
@@ -35,6 +43,12 @@ appDataSource
     console.error(err);
   });
 
+if (env.NODE_ENV !== "development") {
+  // This must be the first piece of middleware in the stack.
+  // It can only capture errors in downstream middleware
+  app.use(bugsnagmiddleware!.requestHandler);
+}
+
 /**
  * Register middlewares
  */
@@ -56,6 +70,11 @@ socketRegisterMiddlewares(remoteControlNamespace);
  * Register remote control connection listener
  */
 remoteControlNamespace.on("connection", remoteControlConnectionListener);
+
+if (env.NODE_ENV !== "development") {
+  // This handles any errors that Express catches (TODO: replace above?)
+  app.use(bugsnagmiddleware!.errorHandler);
+}
 
 // start express server
 server.listen(port, async () => {

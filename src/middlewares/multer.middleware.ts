da92408e9575ff4c811f5d432862df7feaa5a88c
dealparadize <ealejandrocasillas@gmail.com>
2025-03-13 14:41:36 -0700

    fix(multer): validate types with multer

--- src/middlewares/multer.middleware.ts
+++ src/middlewares/multer.middleware.ts
@@ -1,14 +1,37 @@
+import path from "path";
+import multer, { MulterError } from "multer";
 import { GENERAL_MESSAGES } from "constants/messages/general.constants";
 import { NextFunction, Request, Response } from "express";
 import httpStatus from "http-status";
-import multer from "multer";
 import { APP_LOGGER } from "shared/logger";
 import { jsonResponse } from "utils/responses.util";
+import { ALLOWED_IMAGE_TYPES } from "constants/file.constants";
 
 export const multerInstance = multer({
   storage: multer.memoryStorage(),
   limits: {
     fileSize: 25 * 1024 * 1024, // limit file size to 25MB
+  },
+  // fileFilter fn to validate file allowed extensions
+  fileFilter: (_, file, cb) => {
+    // Get file extension removing '.'
+    const ext = path.extname(file.originalname).toLowerCase().slice(1);
+
+    // Validating images only since videos are uploaded directly to s3 using multipart upload
+    const ALLOWED_EXTENSIONS = [...ALLOWED_IMAGE_TYPES];
+
+    // Check if extension is allowed
+    const isExtensionAllowed = ALLOWED_EXTENSIONS.includes(ext);
+
+    if (isExtensionAllowed) {
+      cb(null, true);
+    } else {
+      // Creating not allowed type error
+      const allowedExtsString = ALLOWED_EXTENSIONS.join(", ");
+      const fileTypeError = new MulterError("LIMIT_UNEXPECTED_FILE");
+      fileTypeError.message = `File type not allowed. Allowed extensions: ${allowedExtsString}`;
+      cb(fileTypeError);
+    }
   },
 });
 

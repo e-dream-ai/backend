a1776ae0db93bdecae27ee80aa8284662ca1cf03
Scott Draves <spot@draves.org>
2026-03-05 11:16:26 -0500

    hack quota reset time for testing client

--- src/utils/user.util.ts
+++ src/utils/user.util.ts
@@ -10,7 +10,6 @@ import type { User as WorkOSUser } from "@workos-inc/node";
 import { Role } from "entities/Role.entity";
 import {
   DAILY_USER_DEFAULT_QUOTA,
-  DAILY_USER_QUOTA_RESET_UTC_HOUR,
   MIN_USER_QUOTA,
 } from "constants/user.constants";
 import {
@@ -211,8 +210,8 @@ export const getNextQuotaResetAt = (now: Date = new Date()): Date => {
       now.getUTCFullYear(),
       now.getUTCMonth(),
       now.getUTCDate(),
-      DAILY_USER_QUOTA_RESET_UTC_HOUR,
-      0,
+      16, // DAILY_USER_QUOTA_RESET_UTC_HOUR,
+      30, // 0,
       0,
       0,
     ),
